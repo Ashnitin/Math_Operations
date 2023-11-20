b997e35e6c7b9d78f84a5c205bd735b095fa8d1b
unknown <ashwinijadhav129aj@gmail.com>
2023-11-21 00:29:37 +0530

    [Added]:MathOperationUsingLambdaExpression

--- src/com/mathoperation/MathOperations.java
+++ src/com/mathoperation/MathOperations.java
@@ -1,7 +1,29 @@
 package com.mathoperation;
 
+import java.util.Scanner;
+
+@FunctionalInterface
+interface IMathFuction{
+    int calculate(int a,int b);
+}
 public class MathOperations {
     public static void main(String[] args) {
-        System.out.println("Welcome to Lambda Expression");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your choice: \n1)Addition\n2)Subsraction\n3)Division");
+        int ch =sc.nextInt();
+        switch (ch) {
+            case 1:
+                IMathFuction add = (x, y) -> x + y;
+                System.out.println("Addition is: " + add.calculate(8, 4));
+                break;
+            case 2:
+                IMathFuction sub=(x,y)->x-y;
+                System.out.println("Substraction is: "+sub.calculate(5,4));
+                break;
+            case 3:
+                IMathFuction div=(x,y)->x/y;
+                System.out.println("Division is: "+div.calculate(10,5));
+                break;
+        }
     }
 }
